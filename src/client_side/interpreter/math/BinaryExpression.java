2623d143271d3c72b0e1b5297ccb028bdb6968f7
Erez Ben Moshe <erezbmp@gmail.com>
2020-05-02 11:35:27 +0300

    use DoubleBinaryOperator instead of BiFunction<Double, Double, Double>

--- src/client_side/interpreter/math/BinaryExpression.java
+++ src/client_side/interpreter/math/BinaryExpression.java
@@ -1,29 +1,25 @@
 package client_side.interpreter.math;
 
-import java.util.function.BiFunction;
+import java.util.function.DoubleBinaryOperator;
 
 public class BinaryExpression implements Expression {
     protected Expression left, right;
 
-    private int precedence;
-    private BiFunction<Double,Double,Double> f;
-    public BinaryExpression(BiFunction<Double,Double,Double> f,int precedence) {
-        this.f=f;
-        this.precedence=precedence;
-    }
+    protected final DoubleBinaryOperator operator;
+    protected final int precedence;
 
-    public BinaryExpression(Expression left, Expression right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    public int getPrecedence(){
-        return this.precedence;
+    public BinaryExpression(DoubleBinaryOperator operator, int precedence) {
+        this.operator = operator;
+        this.precedence = precedence;
     }
 
     @Override
-    public double calculate(){
-        return this.f.apply(left.calculate(),right.calculate());
+    public double calculate() {
+        return operator.applyAsDouble(left.calculate(), right.calculate());
+    }
+
+    public int getPrecedence() {
+        return this.precedence;
     }
 
     public Expression getLeft() {
