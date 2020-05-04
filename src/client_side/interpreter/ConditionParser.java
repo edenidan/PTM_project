c231131fa7c2f4c18c21dcca8bac86dd9b6145a5
Erez Ben Moshe <erezbmp@gmail.com>
2020-05-04 11:57:10 +0300

    remove old try catch in ConditionParser and use operators map instead of switch case

--- src/client_side/interpreter/ConditionParser.java
+++ src/client_side/interpreter/ConditionParser.java
@@ -2,6 +2,7 @@ package client_side.interpreter;
 
 import client_side.interpreter.math.ArithmeticParser;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
@@ -10,35 +11,34 @@ public class ConditionParser {
     private ConditionParser() {
     }
 
+    private static final Map<String, MyPredicate> operators = new HashMap<>();
+
     // startIndex: index of the first operand
     public static Boolean parse(List<String> tokens, int startIndex, ConcurrentMap<String, Variable> symbolTable) throws CannotInterpretException {
-        float operand1, operand2;
-        int operand1EndPos;
-        try {
-            operand1 = (float) ArithmeticParser.calc(tokens, startIndex, symbolTable);
-            operand1EndPos = ArithmeticParser.getEndOfExpression(tokens, startIndex,symbolTable);
+        int operand1EndPos = ArithmeticParser.getEndOfExpression(tokens, startIndex, symbolTable);
 
-            operand2 = (float) ArithmeticParser.calc(tokens, operand1EndPos + /*operatorSize*/1 + 1, symbolTable);
-        } catch (NumberFormatException e) {
-            throw new CannotInterpretException("Cannot resolve condition operands", startIndex);
-        }
+        float operand1 = (float) ArithmeticParser.calc(tokens, startIndex, symbolTable);
+        float operand2 = (float) ArithmeticParser.calc(tokens, operand1EndPos + 2, symbolTable);
 
-        String operator = tokens.get(operand1EndPos + 1);
-        switch (operator) {
-            case "==":
-                return operand1 == operand2;
-            case ">=":
-                return operand1 >= operand2;
-            case "<=":
-                return operand1 <= operand2;
-            case ">":
-                return operand1 > operand2;
-            case "<":
-                return operand1 < operand2;
-            case "!=":
-                return operand1 != operand2;
-            default:
-                throw new CannotInterpretException("Unknown operator: " + operator, startIndex);
-        }
+        String operatorToken = tokens.get(operand1EndPos + 1);
+        MyPredicate operator = operators.get(operatorToken);
+        if (operator == null)
+            throw new CannotInterpretException("Unknown operator: " + operatorToken, startIndex);
+
+        return operator.test(operand1, operand2);
+    }
+
+    static {
+        operators.put("==", (a, b) -> a == b);
+        operators.put("!=", (a, b) -> a != b);
+        operators.put("<=", (a, b) -> a <= b);
+        operators.put(">=", (a, b) -> a >= b);
+        operators.put("<", (a, b) -> a < b);
+        operators.put(">", (a, b) -> a > b);
+    }
+
+    // predicate with float parameters, because we cant use Generic interfaces because they don't work with primitives
+    private interface MyPredicate {
+        boolean test(float a, float b);
     }
 }
