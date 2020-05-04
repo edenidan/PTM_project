b434f20a78f4e97b7cfbfb39a865155863e99272
Erez Ben Moshe <erezbmp@gmail.com>
2020-05-04 09:23:59 +0300

    remove setters in PropertyUpdate, isn't supposed to change after creation

--- src/client_side/interpreter/PropertyUpdate.java
+++ src/client_side/interpreter/PropertyUpdate.java
@@ -1,16 +1,19 @@
 package client_side.interpreter;
 
 public class PropertyUpdate {
-    String property;
-    double value;
-    public PropertyUpdate(String property,double value){
-        this.property=property;
+    private final String property;
+    private final double value;
+
+    public PropertyUpdate(String property, double value) {
+        this.property = property;
         this.value = value;
     }
 
+    public String getProperty() {
+        return property;
+    }
 
-    public String getProperty(){return property;}
-    public void setProperty(String property){this.property=property;}
-    public double getValue(){return value;}
-    public void setValue(double value){this.value=value;}
+    public double getValue() {
+        return value;
+    }
 }
