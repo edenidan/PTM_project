75d7bc177b3461398b58d70fbb212c1a41d5aa2c
Erez Ben Moshe <erezbmp@gmail.com>
2020-06-15 00:03:20 +0300

    clean MainWindowView

--- src/client_side/ui/views/MainWindowView.java
+++ src/client_side/ui/views/MainWindowView.java
@@ -1,12 +1,7 @@
 package client_side.ui.views;
 
-import client_side.ui.view_models.MainWindowViewModel;
 import client_side.ui.view_models.MainWindowViewModelImpl;
 
 public interface MainWindowView {
-
-    //void setPlanePos(double xKm,double yKm);
     void setViewModel(MainWindowViewModelImpl vm);
-    //void setPlaneAngle(double heading);
-
 }
