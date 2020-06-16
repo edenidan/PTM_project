fb4ecfd38ecef34a008a0a145c88fb87a2a2115d
Erez Ben Moshe <erezbmp@gmail.com>
2020-06-16 15:32:38 +0300

    put sleep(100) in while so it wouldn'y crunch CPU when no inputs

--- src/client_side/ui/models/MultiSourcePrintWriter.java
+++ src/client_side/ui/models/MultiSourcePrintWriter.java
@@ -6,42 +6,48 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class MultiSourcePrintWriter {
-    private PrintWriter out;
+    private final PrintWriter out;
     private List<BlockingQueue<String>> inputs;
-    private boolean stop=false;
+    private boolean stop = false;
 
-    public MultiSourcePrintWriter(PrintWriter out){
+    public MultiSourcePrintWriter(PrintWriter out) {
         this.out = out;
 
         new Thread(this::backgroundJob).start();
     }
 
-    private void backgroundJob(){
-        while(!stop)
-            for (BlockingQueue<String> q: inputs) {
+    private void backgroundJob() {
+        while (!stop) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
+            for (BlockingQueue<String> q : inputs) {
                 try {
-                    Thread.sleep(300/ inputs.size());//maximum response time of 300 millis
-                } catch (InterruptedException e) { }
+                    Thread.sleep(300 / inputs.size());//maximum response time of 300 millis
+                } catch (InterruptedException ignored) {
+                }
                 String line = q.poll();
-                if(line == null)
+                if (line == null)
                     continue;
                 this.out.println(line);
             }
+        }
     }
 
-    public BlockingQueue<String> getInputChannel(){
+    public BlockingQueue<String> getInputChannel() {
         BlockingQueue<String> newChannel = new LinkedBlockingDeque<>();
         inputs.add(newChannel);
         return newChannel;
     }
 
-    public void unsubscribe(BlockingQueue<String> q){
+    public void unsubscribe(BlockingQueue<String> q) {
         inputs.remove(q);
     }
 
     @Override
-    public void finalize(){
-        this.stop=true;
+    public void finalize() {
+        this.stop = true;
     }
 
 }
