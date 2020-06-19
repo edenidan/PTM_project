2b802e1ed53a40557c653dccf25dfd68fb7e8a72
idaneden <37079533+idaneden@users.noreply.github.com>
2020-06-18 19:32:04 -0700

    fix a*

--- src/server_side/problems/graph_searching/search_algorithms/AStar.java
+++ src/server_side/problems/graph_searching/search_algorithms/AStar.java
@@ -18,34 +18,29 @@ public class AStar<TState> extends CommonSearcher<TState> {
     @Override
     public List<Node<TState>> search(Searchable<TState> searchable) {
 
-        PriorityQueue<Node<TState>> _openList = new PriorityQueue<>(Comparator.comparingDouble((node)->node.getCost() + heuristic.applyAsDouble(node.getState())));
+        PriorityQueue<Node<TState>> _openList = new PriorityQueue<>(Comparator.comparingDouble((node) -> node.getCost() + heuristic.applyAsDouble(node.getState())));
 
         _openList.clear();
         _openList.add(searchable.getInitialNode());
-        Set<Node<TState>> closed = new HashSet<>();
 
         while (!_openList.isEmpty()) {
             Node<TState> node = _openList.poll();
-            closed.add(node);
             if (searchable.isGoalState(node.getState()))
                 return backtrace(node);
 
             //node.setCost(node.getCost() + heuristic.applyAsDouble(node.getState()));
+            _openList.remove(node);
             for (Node<TState> successor : searchable.getAllPossibleNodes(node)) {
                 //successor.setCost(successor.getCost() + heuristic.applyAsDouble(successor.getState()));
                 boolean inOpen = _openList.contains(successor);
                 double alternativeCost = successor.getCostComingFrom(node);
-                if (!closed.contains(successor) && !inOpen) {
-                    successor.setCameFrom(node);
-                    _openList.add(successor);
-                } else if (alternativeCost < successor.getCost()) {
-                    if (inOpen)
-                        _openList.remove(successor);
-                    else
-                        closed.remove(successor);
+                if (alternativeCost < successor.getCost()) {
+                    //_openList.remove(successor);
+
                     successor.setCameFrom(node);
                     successor.setCost(alternativeCost);
-                    _openList.add(successor);
+                    if (!_openList.contains(successor))
+                        _openList.add(successor);
                 }
             }
         }
